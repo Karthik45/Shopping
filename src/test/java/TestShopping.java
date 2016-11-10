c4ce3e5cf13f91b0da80a51c98b6c595128567da
karthik45 <karthikpenujuru@gmail.com>
2016-11-10 15:33:10 +0530

    Added test files

--- src/test/java/TestShopping.java
+++ src/test/java/TestShopping.java
@@ -11,7 +11,7 @@ public class TestShopping {
     Shopping shopping = new Shopping(500.0);
     Item item,item2;
 
-    @Test
+    @Test      //purchase for above 100
     public void testShopping()throws Exception{
 
         item = new ItemsBuilder().withItems(ItemType.APPLE).withPrice(20.0).build();
@@ -23,5 +23,35 @@ public class TestShopping {
         shopping.payMoney();
 
        Assert.assertEquals(shopping.ewallet.getMoney(),253.0);
+
+
+   }
+
+   @Test
+    public void testShopping1()throws Exception{
+
+       item = new ItemsBuilder().withItems(ItemType.APPLE).withPrice(20.0).build();
+       shopping.addItemToCart(item,2);
+
+       item2 = new ItemsBuilder().withItems(ItemType.Milk).withPrice(15.0).build();
+       shopping.addItemToCart(item2,4);
+
+       shopping.payMoney();
+
+       Assert.assertEquals(shopping.ewallet.getMoney(), 405.0);
+   }
+
+   @Test
+    public void testShopping2()throws Exception{
+
+       item = new ItemsBuilder().withItems(ItemType.APPLE).withPrice(20.0).build();
+       shopping.addItemToCart(item,1);
+
+       item2 = new ItemsBuilder().withItems(ItemType.NewsPaper).withPrice(5.0).build();
+       shopping.addItemToCart(item2,4);
+
+       shopping.payMoney();
+
+       Assert.assertEquals(shopping.ewallet.getMoney(), 460.0 );
    }
 }
